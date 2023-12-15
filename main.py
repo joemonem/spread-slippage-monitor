9802b58abd029f5bf9f2bc9adb5910bfcca80904
Joe Monem <jmonem@icloud.com>
2023-12-15 15:18:15 +0200

    implemented pandas for storing data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 import aiohttp
-import csv
+import pandas as pd
 
 # Store 20 popular pairs, available on Binance and Kucoin
 kucoin_trading_pairs = [
@@ -84,6 +84,11 @@ async def fetch_kucoin(session, url, trading_pair):
         # Slippage for sell orders
         slippage_percentage_sell = ((execution_price - best_bid) / best_bid) * 100
 
+        if abs(slippage_percentage_buy) > 2:
+            slippage_percentage_buy = None
+        if abs(slippage_percentage_sell) > 2:
+            slippage_percentage_sell = None
+
         spread_entry = {
             "source": "KuCoin",
             "trading_pair": trading_pair,
@@ -112,6 +117,11 @@ async def fetch_binance(session, url, trading_pair):
 
         # Slippage for sell orders
         slippage_percentage_sell = ((execution_price - best_bid) / best_bid) * 100
+
+        if abs(slippage_percentage_buy) > 2:
+            slippage_percentage_buy = None
+        if abs(slippage_percentage_sell) > 2:
+            slippage_percentage_sell = None
 
         spread_entry = {
             "source": "Binance",
@@ -144,25 +154,24 @@ async def main():
     await get_kucoin()
     await get_binance()
 
-    with open("kucoin_output.csv", "w", newline="") as csvfile:
-        fieldnames = kucoin_results_table[
-            0
-        ].keys()  # Assuming all dictionaries in the list have the same keys
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # Separate data
+    kucoin_data = pd.DataFrame(kucoin_results_table)
+    kucoin_data.set_index(["source", "trading_pair"], inplace=True)
+    binance_data = pd.DataFrame(binance_results_table)
+    binance_data.set_index(["source", "trading_pair"], inplace=True)
 
-        writer.writeheader()
-        for row in kucoin_results_table:
-            writer.writerow(row)
+    # Write to separate CSV files
+    kucoin_data.to_csv("kucoin_output.csv", header=True)
+    binance_data.to_csv("binance_output.csv", header=True)
 
-    with open("binance_output.csv", "w", newline="") as csvfile:
-        fieldnames = binance_results_table[
-            0
-        ].keys()  # Assuming all dictionaries in the list have the same keys
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # Combine results from both tables
+    combined_data = pd.concat([kucoin_data, binance_data])
 
-        writer.writeheader()
-        for row in binance_results_table:
-            writer.writerow(row)
+    # Alternatively, write to a single Excel file with a separate sheet for each source
+    with pd.ExcelWriter("combined_results.xlsx") as writer:
+        combined_data.to_excel(writer, sheet_name="Combined")
+        kucoin_data.to_excel(writer, sheet_name="KuCoin")
+        binance_data.to_excel(writer, sheet_name="Binance")
 
 
 asyncio.run(main())
