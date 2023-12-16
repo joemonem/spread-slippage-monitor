33f18d855842d1b7a5c0391dcfacc2054c7bcd2e
Joe Monem <jmonem@icloud.com>
2023-12-16 11:02:37 +0200

    added comments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import aiohttp
 import pandas as pd
 
-# Store 20 popular pairs, available on Binance and Kucoin
+# Store 20 popular pairs, available on both Binance and Kucoin
 trading_pairs = [
     "BTC-USDT",
     "ETH-USDT",
@@ -31,6 +31,7 @@ kucoin_url = "https://api.kucoin.com/api/v1/market/orderbook/level1?symbol={}"
 
 # Binance ticker GET request
 binance_url = "https://api.binance.com/api/v3/ticker/24hr?symbol={}"
+
 
 kucoin_results_table = []
 binance_results_table = []
@@ -72,6 +73,7 @@ async def fetch_kucoin(session, url, trading_pair):
         best_bid = float(result["data"]["bestBid"])
         best_ask = float(result["data"]["bestAsk"])
 
+        # Spread is the difference between the best ask and bid
         spread = ((best_ask - best_bid) / best_ask) * 100
 
         execution_price = float(result["data"]["price"])
@@ -82,6 +84,7 @@ async def fetch_kucoin(session, url, trading_pair):
         # Slippage for sell orders
         slippage_percentage_sell = ((execution_price - best_bid) / best_bid) * 100
 
+        # Only store the slippage if it's below 2%
         if abs(slippage_percentage_buy) > 2:
             slippage_percentage_buy = None
         if abs(slippage_percentage_sell) > 2:
@@ -148,6 +151,7 @@ async def main():
     # Separate data
     kucoin_data = pd.DataFrame(kucoin_results_table)
     kucoin_data.set_index(["source", "trading_pair"], inplace=True)
+
     binance_data = pd.DataFrame(binance_results_table)
     binance_data.set_index(["source", "trading_pair"], inplace=True)
 
@@ -158,7 +162,7 @@ async def main():
     # Combine results from both tables
     combined_data = pd.concat([kucoin_data, binance_data])
 
-    # Create Excel file with a separate sheet for each source
+    # Create Excel file with a separate sheet for each source, including a sheet for the combination of both sources
     with pd.ExcelWriter("combined_results.xlsx") as writer:
         combined_data.to_excel(writer, sheet_name="Combined")
         kucoin_data.to_excel(writer, sheet_name="KuCoin")
